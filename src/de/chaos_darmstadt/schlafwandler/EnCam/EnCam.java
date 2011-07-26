83ba1388386aabe223cf1a3b37a4f34091436aa8
schlafwandler <you@example.com>
2011-07-26 02:28:00 +0200

    Button on screen added
    preview class no longer in encam.java

--- src/de/chaos_darmstadt/schlafwandler/EnCam/EnCam.java
+++ src/de/chaos_darmstadt/schlafwandler/EnCam/EnCam.java
@@ -1,24 +1,20 @@
 package de.chaos_darmstadt.schlafwandler.EnCam;
 
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.hardware.Camera;
 import android.hardware.Camera.PictureCallback;
-import android.hardware.Camera.Size;
 import android.os.Bundle;
 import android.os.Environment;
-import android.view.Display;
 import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.Surface;
-import android.view.SurfaceHolder;
-import android.view.SurfaceView;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.Toast;
 import de.chaos_darmstadt.schlafwandler.EnCam.R;
 
@@ -27,7 +23,6 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
-import java.util.List;
 import java.util.Random;
 
 // ----------------------------------------------------------------------
@@ -52,8 +47,14 @@ public class EnCam extends Activity {
     	loadSelectedKeys();
 
         // Create our Preview view and set it as the content of our activity.
-        mPreview = new Preview(this);
-        setContentView(mPreview);
+        //mPreview = new Preview(this);
+        //setContentView(mPreview);
+    	
+    	setContentView(R.layout.main);
+    	mPreview = (Preview)findViewById(R.id.preview);
+    	
+    	Button TakePictureButton = (Button)findViewById(R.id.TakePictureButton);
+    	TakePictureButton.getBackground().setAlpha(100);
         
         // new random number generator (not secure, for file names only)
         mPRNG = new Random();
@@ -118,6 +119,12 @@ public class EnCam extends Activity {
     	}
     	else
     		return super.onKeyDown(keyCode, event);    	
+	}
+	
+	public void onTakePictureButton(View v)
+	{
+		Camera mCamera = mPreview.getCamera();
+		mCamera.takePicture(null, null, this.onPictureTakenJPEG);
 	}
     
 	@Override
@@ -278,113 +285,3 @@ public class EnCam extends Activity {
 
 // ----------------------------------------------------------------------
 
-class Preview extends SurfaceView implements SurfaceHolder.Callback {
-    SurfaceHolder mHolder;
-    Context context;
-    public Camera mCamera;
-
-    
-    Preview(Context context) {
-        super(context);
-        this.context = context;
-
-        // Install a SurfaceHolder.Callback so we get notified when the
-        // underlying surface is created and destroyed.
-        mHolder = getHolder();
-        mHolder.addCallback(this);
-        mHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
-    }
-        
-    public Camera getCamera()
-    {
-    	return this.mCamera;
-    }
-    
-    
-    public void surfaceCreated(SurfaceHolder holder) {
-        // The Surface has been created, acquire the camera and tell it where
-        // to draw.
-    	mCamera = Camera.open();
-    	
-        try {
-           mCamera.setPreviewDisplay(holder);
-        } catch (IOException exception) {
-            mCamera.release();
-            mCamera = null;
-        }
-    }
-
-    public void surfaceDestroyed(SurfaceHolder holder) {
-        // Surface will be destroyed when we return, so stop the preview.
-        // Because the CameraDevice object is not a shared resource, it's very
-        // important to release it when the activity is paused.
-    	if (mCamera != null)
-        {
-//    		mCamera.setPreviewCallback(null);
-    		mCamera.stopPreview();       
-    		mCamera.release();
-    		mCamera = null;
-        }
-    }
-
-
-    private Size getOptimalPreviewSize(List<Size> sizes, int w, int h) {
-        final double ASPECT_TOLERANCE = 0.05;
-        double targetRatio = (double) w / h;
-        if (sizes == null) return null;
-
-        Size optimalSize = null;
-        double minDiff = Double.MAX_VALUE;
-
-        int targetHeight = h;
-
-        // Try to find an size match aspect ratio and size
-        for (Size size : sizes) {
-            double ratio = (double) size.width / size.height;
-            if (Math.abs(ratio - targetRatio) > ASPECT_TOLERANCE) continue;
-            if (Math.abs(size.height - targetHeight) < minDiff) {
-                optimalSize = size;
-                minDiff = Math.abs(size.height - targetHeight);
-            }
-        }
-
-        // Cannot find the one match the aspect ratio, ignore the requirement
-        if (optimalSize == null) {
-            minDiff = Double.MAX_VALUE;
-            for (Size size : sizes) {
-                if (Math.abs(size.height - targetHeight) < minDiff) {
-                    optimalSize = size;
-                    minDiff = Math.abs(size.height - targetHeight);
-                }
-            }
-        }
-        return optimalSize;
-    }
-
-    public void surfaceChanged(SurfaceHolder holder, int format, int w, int h) {
-    	Display display = ((WindowManager)context.getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay();
-        Camera.Parameters parameters = mCamera.getParameters();
-
-        List<Size> sizes = parameters.getSupportedPreviewSizes();
-        Size optimalSize = getOptimalPreviewSize(sizes, w, h);
-    	parameters.setPreviewSize(optimalSize.width, optimalSize.height);
-
-        switch (display.getRotation())
-        {
-        case Surface.ROTATION_0:
-        	mCamera.setDisplayOrientation(90);
-        	break;
-        case Surface.ROTATION_90:
-        	break;
-        case Surface.ROTATION_180:
-        	break;
-        case Surface.ROTATION_270:
-        	mCamera.setDisplayOrientation(180);
-        	break;
-        }
-        
-        mCamera.setParameters(parameters);
-        mCamera.startPreview();
-    }
-
-}
---
+++ src/de/chaos_darmstadt/schlafwandler/EnCam/Preview.java
@@ -0,0 +1,125 @@
+package de.chaos_darmstadt.schlafwandler.EnCam;
+
+import java.io.IOException;
+import java.util.List;
+
+import android.content.Context;
+import android.hardware.Camera;
+import android.hardware.Camera.Size;
+import android.util.AttributeSet;
+import android.view.Display;
+import android.view.Surface;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.WindowManager;
+
+class Preview extends SurfaceView implements SurfaceHolder.Callback {
+    SurfaceHolder mHolder;
+    Context context;
+    public Camera mCamera;
+
+    public Preview(Context context,AttributeSet attrs) {
+    	super(context,attrs);
+
+        this.context = context;
+
+        // Install a SurfaceHolder.Callback so we get notified when the
+        // underlying surface is created and destroyed.
+        mHolder = getHolder();
+        mHolder.addCallback(this);
+        mHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+    }
+        
+    public Camera getCamera()
+    {
+    	return this.mCamera;
+    }
+    
+    
+    public void surfaceCreated(SurfaceHolder holder) {
+        // The Surface has been created, acquire the camera and tell it where
+        // to draw.
+    	mCamera = Camera.open();
+    	
+        try {
+           mCamera.setPreviewDisplay(holder);
+        } catch (IOException exception) {
+            mCamera.release();
+            mCamera = null;
+        }
+    }
+
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        // Surface will be destroyed when we return, so stop the preview.
+        // Because the CameraDevice object is not a shared resource, it's very
+        // important to release it when the activity is paused.
+    	if (mCamera != null)
+        {
+//    		mCamera.setPreviewCallback(null);
+    		mCamera.stopPreview();       
+    		mCamera.release();
+    		mCamera = null;
+        }
+    }
+
+
+    private Size getOptimalPreviewSize(List<Size> sizes, int w, int h) {
+        final double ASPECT_TOLERANCE = 0.05;
+        double targetRatio = (double) w / h;
+        if (sizes == null) return null;
+
+        Size optimalSize = null;
+        double minDiff = Double.MAX_VALUE;
+
+        int targetHeight = h;
+
+        // Try to find an size match aspect ratio and size
+        for (Size size : sizes) {
+            double ratio = (double) size.width / size.height;
+            if (Math.abs(ratio - targetRatio) > ASPECT_TOLERANCE) continue;
+            if (Math.abs(size.height - targetHeight) < minDiff) {
+                optimalSize = size;
+                minDiff = Math.abs(size.height - targetHeight);
+            }
+        }
+
+        // Cannot find the one match the aspect ratio, ignore the requirement
+        if (optimalSize == null) {
+            minDiff = Double.MAX_VALUE;
+            for (Size size : sizes) {
+                if (Math.abs(size.height - targetHeight) < minDiff) {
+                    optimalSize = size;
+                    minDiff = Math.abs(size.height - targetHeight);
+                }
+            }
+        }
+        return optimalSize;
+    }
+
+    public void surfaceChanged(SurfaceHolder holder, int format, int w, int h) {
+    	Display display = ((WindowManager)context.getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay();
+        Camera.Parameters parameters = mCamera.getParameters();
+
+        List<Size> sizes = parameters.getSupportedPreviewSizes();
+        Size optimalSize = getOptimalPreviewSize(sizes, w, h);
+    	parameters.setPreviewSize(optimalSize.width, optimalSize.height);
+
+        switch (display.getRotation())
+        {
+        case Surface.ROTATION_0:
+        	mCamera.setDisplayOrientation(90);
+        	break;
+        case Surface.ROTATION_90:
+        	break;
+        case Surface.ROTATION_180:
+        	break;
+        case Surface.ROTATION_270:
+        	mCamera.setDisplayOrientation(180);
+        	break;
+        }
+        
+        mCamera.setParameters(parameters);
+        mCamera.startPreview();
+    }
+
+}
